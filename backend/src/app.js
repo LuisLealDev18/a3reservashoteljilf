012198386fd6d8ff8b1a1e08a46035dd991c556d
Jeffesson Gomes <jeffessong4@gmail.com>
2025-11-25 12:02:45 -0300

    Delete backend directory

--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookingRoutes = require('./routes/bookingRoutes');
-
-const app = express();
-app.use(express.json());
-
-app.use('/bookings', bookingRoutes);
-
-module.exports = app;
--- backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/db');
-const Booking = require('./Booking');
-
-// Associações podem ir aqui, se houver
-module.exports = {
-  sequelize,
-  Booking
-};
