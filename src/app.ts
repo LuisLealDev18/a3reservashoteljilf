ef9d93464df63eeb04706ead116d795579b473d8
Jeffesson Gomes <jeffessongomes@MacBook-Air-de-Jeffesson.local>
2025-11-27 00:07:48 -0300

    feature: creating route to create users

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,6 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
-app.post('/users', async (request, reply) => {
-  const registerBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    cpf: z.string(),
-    telephone: z.string(),
-    address: z.string(),
-    course: z.string(),
-    password: z.string().min(6),
-    status: z.string(),
-  })
-
-  const { name, email, cpf, telephone, address, course, password, status } =
-    registerBodySchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      cpf,
-      telephone,
-      address,
-      course,
-      password,
-      status,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.register(appRoutes)
