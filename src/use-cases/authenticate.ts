4a8a8053ebb292a84a1caa665861ba7dc0989434
Jeffesson Gomes <jeffessongomes@MacBook-Air-de-Jeffesson.local>
2025-11-27 23:08:36 -0300

    feature: create authentication of student

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,36 @@
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import { Student } from '@prisma/client'
+import { StudentsRepository } from 'src/repositories/students-repository'
+
+interface AuthenticateUseCaseRequest {
+  username: string
+  password: string
+}
+
+type AuthenticateUseCaseResponse = {
+  student: Student
+}
+
+export class AuthenticateUseCase {
+  constructor(private studentsRepository: StudentsRepository) {}
+
+  async execute({
+    username,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const student = await this.studentsRepository.findByEmail(username)
+
+    if (!student) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, student.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { student }
+  }
+}
